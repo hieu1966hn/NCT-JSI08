1cbad0acac62a3bbcc107dd06b63e7783166b493
hieu1966hn <hieu1966hn@gmail.com>
2022-09-24 20:11:15 +0700

    new code

---
+++ SS2/app.js
@@ -0,0 +1,49 @@
+///// Ôn tập kiến thức tại file này..
+
+/*
+Ngôn ngữ lập trình là gì? 
+- HTML, CSS => không được gọi là ngôn ngữ lập trình
+- Javascript là ngôn ngữ lập trình thôi
+- Nn lập trình là ngôn ngữ có thể xử lý logic, tính năng 
+
+
+
+Trong JS em đã học những gì về nó?
+- Hàm
+- Array: 0
+- Object: 0
+- Câu điều kiện
+- Loop: 0
+- LocalStorage
+- Async await: 0
+- Apis
+- Promise
+- JSON
+- DOM
+- Biến
+*/
+
+
+/*
+Kiểu dữ liệu: 6 kiểu dữ liệu cơ bản Trong JS
+- Number: Số
+- String:  Chuỗi
+- Null: Rỗng
+- Object: Đối tượng
+- Boolean: true/false
+- Undefinded: Không xác định
+
+
+Biến là gì?
+- Khái niệm: Biến là vùng nhớ dùng để lưu trữ dữ liệu
+
+Cú pháp khai báo biến: let/ var/ const
+- let/var: dùng để khai báo tên biến bình thường
+- const: Dùng để khai báo hằng số (hằng số thì không thay đổi được)
+*/
+
+let a = 1;
+let b = 2;
+console.log("a bằng: ", a); // dùng để in giá trị|| dữ liệu lên màn hình console
+console.log("b bằng: ", b);
+
