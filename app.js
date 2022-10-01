810a5159252e11f0340bc8e365bddf20b3e0abf4
hieu1966hn <hieu1966hn@gmail.com>
2022-10-01 20:01:52 +0700

    new code DOM

---
+++ app.js
@@ -0,0 +1,39 @@
+/*
+Buổi trước mình học về gì: Ôn tập syntax loop, câu đk, kiến thức căn bản của JS
+thao tác với arr: splice, C,R,U,D
+
+
+
+Buổi 3:
+- Reduce
+- split
+- slice
+- map
+- filter
+- find
+- indexOf
+*/
+
+// VD về dom
+// const h1 = document.getElementById("demo");
+// console.log("h1: ", h1);
+
+
+// const chao = document.querySelector(".chao");
+// console.log("chao: ", chao);
+
+
+// const query = document.querySelector("#demo")
+// query.innerHTML = "chao xin"
+// console.log("query: ", query);
+
+// VD về reduce:
+// const numbers = [175, 50, 25];
+
+// document.getElementById("demo").innerHTML = numbers.reduce(myFunc);
+
+// function myFunc(total, num) {
+//   return total - num;
+// }
+
+
