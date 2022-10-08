5856e9422441cd7245bdd6fd61cebf7adc6db69d
hieu1966hn <hieu1966hn@gmail.com>
2022-10-08 21:14:34 +0700

    new code Kiểu dữ liệu

--- SS4/app.js
+++ SS4/app.js
@@ -114,19 +114,50 @@ Note: Dùng reduce
 
 //////// Lập trình hướng đối tượng: OOP - Object Oriented Programming)
 ///////  Khởi tạo classes: => đơn giản giống như ta đang định nghĩa 1 KDL mới trong dự án
+// class Person {
+
+//   walk() {
+//     alert("Tôi đang đi bộ trên đường Trường Sơn");
+//   }
+
+//   sleep() {
+//     alert("Quỳnh Như đang ngủ đấy, đừng dại gọi nhé")
+//   }
+// }
+
+// let QuynhNhu = new Person();
+// QuynhNhu.walk();
+// QuynhNhu.sleep();
+
+
+
+////// Khởi tạo thuộc tính trong class
 class Person {
-
-  walk() {
-    alert("Tôi đang đi bộ trên đường Trường Sơn");
+  ///// Khai báo thuộc tính ta sử dụng cú pháp contructor
+  constructor(name, age, address, school) {
+    /// phải gán giá trị được nhập vào 
+    /// Thuộc tính của Person là: _name, _age, .._school
+    this._name = name;
+    this._age = age;
+    this._address = address;
+    this._school = school
   }
 
-  sleep() {
-    alert("Quỳnh Như đang ngủ đấy, đừng dại gọi nhé")
+
+  /// Phương thức của Person
+  goToSchool() {
+    alert("Duy Long đang đi học đấy")
   }
+
+  eat() {
+    alert("Duy Long đang ăn sáng")
+  }
+
 }
 
-let QuynhNhu = new Person();
-QuynhNhu.walk();
-QuynhNhu.sleep();
+// let/var/const
+let DuyLong = new Person("Nguyễn Trần Duy Long", 16, "Từ Sơn - Bắc Ninh", "Ngô Gia Tự");
+console.log("DuyLong: ", DuyLong);
+
 
 
