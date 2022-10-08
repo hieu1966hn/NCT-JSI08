afedbbc2d194152b88979178bbee638da9f109a2
hieu1966hn <hieu1966hn@gmail.com>
2022-10-08 20:39:46 +0700

    done bài 3

--- SS4/app.js
+++ SS4/app.js
@@ -37,17 +37,51 @@ Buổi 3 học gì?
 ///////////// Chữa BTVN
 //// Bài 1: 
 /*
-Cho 2 mảng sau: arr1 = [1,2,4,5,6]; arr2 = [1.6.8.9.0]. 
-Hãy lọc ra một mảng mới chứa 2 phần tử trùng nhau của 2 mảng cho bên trên. 
+Cho 2 mảng sau: arr1 = [1,2,4,5,6]; arr2 = [1.6.8.9.0].
+Hãy lọc ra một mảng mới chứa 2 phần tử trùng nhau của 2 mảng cho bên trên.
 (sử dụng filter() )
  */
 
-let arr1 = [1, 2, 4, 5, 6];
-let arr2 = [1, 6, 8, 9, 0];
-let arr3 = arr1.filter(item => arr2.includes(item));/// true/falsev => true, false, false, ..., true 
-console.log("arr3: ", arr3); // [1, 6]
+// let arr1 = [1, 2, 4, 5, 6];
+// let arr2 = [1, 6, 8, 9, 0];
+// let arr3 = arr1.filter(item => arr2.includes(item));/// true/falsev => true, false, false, ..., true 
+// console.log("arr3: ", arr3); // [1, 6]
 
 
 // console.log(
 //   arr2.includes(10)
 // );
+
+
+
+
+////// Chữa bài 2:
+/*
+Sử dụng map() với arr = [1,54,6,7]
+để tạo ra một newArr có newArr[i] = arr[i] + 5.
+*/
+// let arr = [1, 54, 6, 7];
+// let newArr = arr.map(item => item + 5)
+// console.log("newArr: ", newArr);
+
+
+///// Chữa bài 3: 
+/*
+Cho array sau: 
+m = [1,2,4,5,6,7]; 
+n = [3,5,675,8,96]. 
+Hãy viết một hàm, duyệt cả các mảng m và n; 
+loại bỏ đi phần tử có giá trị bằng 1, 8,10,96,7.
+
+*/
+let m = [1, 2, 4, 5, 6, 7];
+let n = [3, 5, 675, 8, 96];
+
+function boGiaTri(arr) {
+  return arr.filter(item => ![1, 8, 10, 96, 7].includes(item))
+
+}
+
+console.log(
+  boGiaTri(n)
+);
