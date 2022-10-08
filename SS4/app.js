b17bb756db78d8152241599c226cd2d06e4c3452
hieu1966hn <hieu1966hn@gmail.com>
2022-10-08 19:56:01 +0700

    Done ôn lại map

--- SS4/app.js
+++ SS4/app.js
@@ -15,3 +15,11 @@ Buổi 3 học gì?
 
 // console.log(sumWithInitial);
 // // expected output: 10
+
+
+/////// map là hàm dùng với mảng => kết quả trả về là 1 mảng mới đã thực thi câu lệnh với từng phần tử;
+
+///// Đề bài tăng toàn bộ phần tử mảng arr gấp 5 lần ban đầu và in ra nó:
+// let arr = [1, 2, 3, 4, 5]; // [5,10,15,20,25]
+// let arrMap = arr.map(item => item *5);// [5,10,15,20,25]
+// console.log("arrMap: ", arrMap);
