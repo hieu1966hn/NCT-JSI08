04d240e51ff7bffa9b9d1040dec6a1032494f323
hieu1966hn <hieu1966hn@gmail.com>
2022-10-29 20:23:03 +0700

    New code bài 10

--- SS7/app.js
+++ SS7/app.js
@@ -1,7 +1,24 @@
-let m = [1, 2, 3, 4, 5, 6, "hh", "9", 80, 100];
-/// Filter lọc mảng
-let mNumber = m.filter(item => typeof (item) === "number")
-console.log("mNumber: ", mNumber);
+// let m = [1, 2, 3, 4, 5, 6, "hh", "9", 80, 100];
+// /// Filter lọc mảng
+// let mNumber = m.filter(item => typeof (item) === "number")
+// console.log("mNumber: ", mNumber);
 
-let mSquared = mNumber.map(item => item * item)
-console.log("mSquared: ", mSquared);
+// let mSquared = mNumber.map(item => item * item)
+// console.log("mSquared: ", mSquared);
+
+
+/*
+Câu 10: Thực hiện duyệt string sau đây: “ High knowledge, high return”. 
+Chuyển đổi string về dạng array, xóa bỏ các phần tử có giá trị là “ “; 
+và các phần tử có giá trị là chữ hoa chuyển thành chữ thường.
+*/
+let str = " High knowledge, high return";
+// xử lý chữ hoa về chữ thường 
+str = str.toLowerCase().trim();
+console.log("str: ", str);
+
+// Chuyển đổi String về dạng mảng split()
+let NewArrayStr = str.split(" ");
+console.log("NewArrayStr: ", NewArrayStr);
+
+
